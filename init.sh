e1698d2537fefaa6982a178f6b5330a7c57d63a8
kopalja <kopal.jakub@gmail.com>
2022-08-27 20:27:03 +0200

    Fix init

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ rm -rf ~/.oh-my-zsh
 
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+exit
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 cp zshrc ~/.zshrc
