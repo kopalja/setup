8ef1cecf211cd9056b7e4419cd00485440aa35c9
kopalja <kopal.jakub@gmail.com>
2022-08-27 19:53:38 +0200

    Add config files

---
+++ init.sh
@@ -0,0 +1,7 @@
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+cp zshrc ~/.zshrc
+cp p10k.zsh ~/.p10k.zsh
