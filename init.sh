6a03ca899352b1ae69096c017a49abfc228c92a0
kopalja <kopal.jakub@gmail.com>
2022-08-28 16:23:35 +0200

    Remoe power 10k

--- init.sh
+++ init.sh
@@ -12,4 +12,3 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 cat zshrc >> ~/.zshrc
-cp p10k.zsh ~/.p10k.zsh
