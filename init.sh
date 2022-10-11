5c09c88b98fe94ed5ba7567f55d876c5263ebc21
kopalja <kopal.jakub@gmail.com>
2022-10-11 21:27:10 +0200

    fix vim plugin

--- init.sh
+++ init.sh
@@ -11,6 +11,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/loket/oh-my-zsh/feature/ba
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# Install neovim plugin manager
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 cat zshrc >> ~/.zshrc
 cat vimrc >> ~/.vimrc
 cat tmux.conf >> ~/.tmux.conf
